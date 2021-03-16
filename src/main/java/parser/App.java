c6f831f2ed515cc34045a685b87a8bbd58ea8c19
requin-citron <requin_citron@protonmail.com>
2021-03-16 12:48:52 +0100

    asm-parser marche bien

--- src/main/java/parser/App.java
+++ src/main/java/parser/App.java
@@ -8,14 +8,14 @@ public class App
 {
     public static void main( String[] args )
     {
-        if(args.length < 1){
-          System.out.println("Usage: file_name.jar file.red");
+        if(args.length < 2){
+          System.out.println("Usage: file_name.jar file.red ouput");
           System.exit(1);
         }
 
         String path = args[0];
         System.out.println("Processing: "+path);
-        String pathWrite = "./" + "out.cor";
+        String pathWrite = args[1];
         ParserFile parser = new ParserFile(path);
         FileCompiler output = new FileCompiler(pathWrite);
         String line = parser.readLine();
